4cf98c7c2f66c90964ec25e48e6d70aa746f0f84
pradnya-11 <pradnyayadav21104@gmail.com>
2024-08-28 11:56:37 +0530

    pushed

---
+++ day2/program2.java
@@ -0,0 +1,14 @@
+class Demo{
+
+	public static void main(String[] args){
+		int a=1;
+		while(a<=15){
+		if(a%2==0){
+			System.out.println(a+"number is even");
+	}else{
+		System.out.println(a+"number is odd");
+				}
+				a++;
+			}
+		}
+	}
---
+++ day2/program5.java
@@ -0,0 +1,13 @@
+class Demo{
+	public static void main(String[] args){
+
+	int a=1;
+	while(a<=15){
+	if(a%3==0 && a%7==0){
+		System.out.println(a +" ");
+
+	}
+	a++;
+		}
+	}	
+}
